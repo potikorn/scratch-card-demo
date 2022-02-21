bf94590734b35e9cd60871b7aac77576242ec0ab
Best <potikornth@gmail.com>
2022-02-21 08:46:35 +0700

    chore: adjust layout

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,11 @@ class FullScreenDialog extends StatelessWidget {
                   icon: const Icon(Icons.close),
                   color: Colors.grey[400]),
             ),
-            childWidget,
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: childWidget,
+            )),
           ],
         ),
       ),
@@ -96,98 +100,92 @@ class _ScratchCardState extends State<ScratchCard> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              decoration: BoxDecoration(
-                  border: Border.all(width: 4, color: Colors.white),
-                  borderRadius: BorderRadius.circular(12.0)),
-              child: const Text(
-                'B',
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.white, fontSize: 56),
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Get the product for free',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Wipe off the protective layer and put the free product in the shopping basket',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 24),
-            Container(
-              clipBehavior: Clip.hardEdge,
-              decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(8.0))),
-              child: Scratcher(
-                key: scratchKey,
-                brushSize: 30,
-                threshold: 70,
-                image: Image.asset('assets/scratch_cover.png'),
-                onChange: (value) => print("Scratch progress: $value%"),
-                onThreshold: () {
-                  print("Threshold reached, you won!");
-                  setState(() {
-                    canAdd = true;
-                  });
-                },
-                child: Container(
-                  height: 300,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      image: DecorationImage(
-                        image: AssetImage('assets/golden_ticket.png'),
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(8.0))),
-                ),
-              ),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {
-                canAdd ? Fluttertoast.showToast(msg: 'Added to cart') : null;
-              },
-              child: const Text(
-                'Add product to cart',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18),
-              ),
-              style: ElevatedButton.styleFrom(
-                  primary: canAdd ? Colors.cyan[300] : Colors.grey,
-                  minimumSize: const Size.fromHeight(50)),
-            ),
-            TextButton(
-              onPressed: () {
-                scratchKey.currentState
-                    ?.reset(duration: const Duration(milliseconds: 500));
-                setState(() {
-                  canAdd = false;
-                });
-              },
-              child: const Text(
-                'RESET',
-                style: TextStyle(color: Colors.white),
-              ),
-            )
-          ],
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: BoxDecoration(
+              border: Border.all(width: 4, color: Colors.white),
+              borderRadius: BorderRadius.circular(12.0)),
+          child: const Text(
+            'B',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white, fontSize: 56),
+          ),
         ),
-      )),
+        const SizedBox(height: 10),
+        const Text(
+          'Get the product for free',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+        const SizedBox(height: 10),
+        const Text(
+          'Wipe off the protective layer and put the free product in the shopping basket',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white),
+        ),
+        const SizedBox(height: 24),
+        Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: const BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          child: Scratcher(
+            key: scratchKey,
+            brushSize: 30,
+            threshold: 50,
+            image: Image.asset('assets/scratch_cover.png'),
+            onChange: (value) => print("Scratch progress: $value%"),
+            onThreshold: () {
+              print("Threshold reached, you won!");
+              scratchKey.currentState
+                  ?.reveal(duration: const Duration(milliseconds: 1500));
+              setState(() {
+                canAdd = true;
+              });
+            },
+            child: Container(
+              height: 300,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                    image: AssetImage('assets/golden_ticket.png'),
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0))),
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        ElevatedButton(
+          onPressed: () {
+            canAdd ? Fluttertoast.showToast(msg: 'Added to cart') : null;
+          },
+          child: const Text(
+            'Add product to cart',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+          ),
+          style: ElevatedButton.styleFrom(
+              primary: canAdd ? Colors.cyan[300] : Colors.grey,
+              minimumSize: const Size.fromHeight(50)),
+        ),
+        TextButton(
+          onPressed: () {
+            scratchKey.currentState
+                ?.reset(duration: const Duration(milliseconds: 500));
+            setState(() {
+              canAdd = false;
+            });
+          },
+          child: const Text(
+            'RESET',
+            style: TextStyle(color: Colors.white),
+          ),
+        )
+      ],
     );
   }
 }
